993e0f3ea82bbe9fa9b9414b98dd6965ca20f206
nabeelcodes <nabeeldesigner91@hotmail.com>
2025-05-31 00:20:16 +0530

    Refactored database migration paths.
    - Modified drizzle.config.ts for the correct path.
    - Modified package.json for the correct path.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
 	dialect: 'sqlite',
 	schema: './src/db/schema',
-	out: './drizzle/migrations',
+	out: './src/db/migrations',
 	dbCredentials: {
 		url: 'file:./.wrangler/state/v3/d1/miniflare-D1DatabaseObject/8fc43331c3bc5f87a139fcd20e65220a2ec74dd3e026018faa3ecc23a798ee0b.sqlite'
 	}
